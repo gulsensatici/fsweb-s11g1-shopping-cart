f1fc16e4d091c0ca37a97b4213cdc2cd9de0f077
Gülşen Satıcı <125642521+gulsensatici@users.noreply.github.com>
2023-05-01 21:37:32 +0300

    tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
-
+import { ProductContext } from "./contexts/ProductContext";
+import {CartContext} from "./contexts/CartContext"
 // Bileşenler
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-  const [products, setProducts] = useState(data);
-  const [cart, setCart] = useState([]);
+  const [products, setProducts] = React.useState(data);
+  const [cart, setCart] =React.useState([]);
 
   const addItem = (item) => {
+    const newCart= [ 
+      ...cart,
+    item];
+    setCart(newCart);
+
     // verilen itemi sepete ekleyin
   };
 
+  const removeItem=(id)=>{
+    const newCart= cart.filter((item)=>item.id !==id);
+    setCart(newCart);
+
+  };
+
   return (
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{cart, removeItem}}>
     <div className="App">
-      <Navigation cart={cart} />
+      <Navigation  />
 
       {/* Routelar */}
       <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
+      <Route exact path="/">
+  <Products />
+</Route>
 
         <Route path="/cart">
-          <ShoppingCart cart={cart} />
+          <ShoppingCart/>
         </Route>
       </main>
     </div>
+      </CartContext.Provider>
+      </ProductContext.Provider>
   );
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-// Components
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
 import Product from "./Product";
 
 const ScProducts = styled.section`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1.5rem;
-
   @media (max-width: 480px) {
     grid-template-columns: repeat(1, 1fr);
-
     img {
       width: 100%;
     }
   }
 `;
-
-const Products = (props) => {
+const Products = () => {
+  const {products,addItem}= useContext(ProductContext);
   return (
     <ScProducts>
-      {props.products.map((product) => (
-        <Product key={product.id} product={product} addItem={props.addItem} />
+       {products.map((product) => (
+        <Product key={product.id} product={product} addItem={addItem} />
       ))}
     </ScProducts>
   );
